71d9e09049b5f950acfb2f87fda8db28d58061f9
Romulo <romulosilva001@gmail.com>
2023-03-11 12:20:25 -0300

    :sparkles: feat: Iniciado o tratamento de exceções no controller

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -4,18 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\DogModel;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class DogController extends Controller
 {
     public function index()
     {
-        return DogModel::all();
+        try {
+            $dogs = DogModel::all();
+            return response()->json($dogs, 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Nenhum registro encontrado na base dados!', 'error' => $e->getMessage()], 500);
+        }
     }
 
     public function show(DogModel $dog)
     {
-        return $dog;
+        try {
+            return response()->json($dog, 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Dog não encontrado!', 'error' => $e->getMessage()], 422);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Ocorreu um erro ao processar a solicitação!', 'error' => $e->getMessage()], 500);
+        }
     }
 
     public function store(Request $request)
