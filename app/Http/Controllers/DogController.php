fe95983b8335ca583f2a089e3919891a9bf99f94
Romulo <romulosilva001@gmail.com>
2023-03-14 21:15:40 -0300

    :sparkles: feat: Criado form request para validação

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DogRequest;
 use App\Models\DogModel;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -12,31 +13,41 @@ class DogController extends Controller
 {
     public function index()
     {
-        try {
-            $dogs = DogModel::all();
-            return response()->json($dogs, 200);
-        } catch (Exception $e) {
-            return response()->json(['message' => 'Nenhum registro encontrado na base dados!', 'error' => $e->getMessage()], 500);
-        }
+        $dogs = DogModel::all();
+        return response()->json($dogs, 200);
+        // if ($dogs === []) {
+        //     return response()->json($dogs, 200);
+        // } else {
+        //     return response()->json(['message' => 'Nenhum registro encontrado na base dados!'], 500);
+        // }
+
+
+        // try {
+        //     $dogs = DogModel::all();
+        //     return response()->json($dogs, 200);
+        // } catch (Exception $e) {
+        //     return response()->json(['message' => 'Nenhum registro encontrado na base dados!', 'error' => $e->getMessage()], 500);
+        // }
     }
 
     public function show(DogModel $dog)
     {
-        try {
-            return response()->json($dog, 200);
-        } catch (ModelNotFoundException $e) {
-            return response()->json(['message' => 'Dog não encontrado!', 'error' => $e->getMessage()], 422);
-        } catch (Exception $e) {
-            return response()->json(['message' => 'Ocorreu um erro ao processar a solicitação!', 'error' => $e->getMessage()], 500);
-        }
+        return response()->json($dog, 200);
+        // try {
+        //     return response()->json($dog, 200);
+        // } catch (ModelNotFoundException $e) {
+        //     return response()->json(['message' => 'Dog não encontrado!', 'error' => $e->getMessage()], 422);
+        // } catch (Exception $e) {
+        //     return response()->json(['message' => 'Ocorreu um erro ao processar a solicitação!', 'error' => $e->getMessage()], 500);
+        // }
     }
 
-    public function store(Request $request)
+    public function store(DogRequest $request)
     {
         return DogModel::create($request->all());
     }
 
-    public function update(Request $request, DogModel $dog)
+    public function update(DogRequest $request, DogModel $dog)
     {
         $dog->update($request->all());
         return $dog;
